12810f990b70fdce992a3f417ddda632c4a8c2f2
Felipe Rodrigues <felipe@crafters.cc>
2020-02-09 19:19:24 -0300

    Implementing the take_concentrations function with its tests
    It receives a grid and a count of how many concentrations it wants in return. This function will calculate the concentration based on the surroundings of each location.

--- lib/water_rover.ex
+++ lib/water_rover.ex
@@ -1,2 +1,50 @@
 defmodule WaterRover do
+  @moduledoc """
+  `WaterRover` is a module to process the mars rover water sensors to find water concentration in mars.
+  """
+
+  @doc """
+  Returns a list containing the `count` biggest concentrations in the `grid` with the index in the format [x, y, score: sum].
+
+  ## Examples
+      iex> grid = [[5, 3, 1, 2], [4, 1, 1, 3], [2, 3, 2, 4], [0, 2, 3, 4]]
+      iex> WaterRover.take_concentrations(grid, 3)
+      [
+        [2, 2, score: 23],
+        [1, 1, score: 22],
+        [1, 2, score: 20]
+      ]
+  """
+  def take_concentrations(grid, count) do
+    comparator = &({Enum.at(&1, 2), -Enum.at(&1, 0)} >= {Enum.at(&2, 2), -Enum.at(&2, 0)})
+    size = Enum.count(grid)
+
+    for {row, row_idx} <- Enum.with_index(grid),
+        {_, col} <- Enum.with_index(row),
+        into: Heap.new(comparator) do
+      # into: Heap.new(&comparator/2) do
+
+      left_idx = if row - 1 < 0, do: size, else: row - 1
+      top_idx = if col - 1 < 0, do: size, else: col - 1
+
+      surroundings = [
+        grid |> Enum.at(left_idx, []) |> Enum.at(top_idx, 0),
+        grid |> Enum.at(row, []) |> Enum.at(top_idx, 0),
+        grid |> Enum.at(row + 1, []) |> Enum.at(top_idx, 0),
+        grid |> Enum.at(left_idx, []) |> Enum.at(col, 0),
+        grid |> Enum.at(row, []) |> Enum.at(col, 0),
+        grid |> Enum.at(row + 1, []) |> Enum.at(col, 0),
+        grid |> Enum.at(left_idx, []) |> Enum.at(col + 1, 0),
+        grid |> Enum.at(row, []) |> Enum.at(col + 1, 0),
+        grid |> Enum.at(row + 1, []) |> Enum.at(col + 1, 0)
+      ]
+
+      [
+        row_idx,
+        col,
+        score: surroundings |> Enum.sum()
+      ]
+    end
+    |> Enum.take(count)
+  end
 end
--- test/water_rover_test.exs
+++ test/water_rover_test.exs
@@ -2,4 +2,42 @@ defmodule WaterRoverTest do
   use ExUnit.Case
   doctest WaterRover
 
+  @moduledoc false
+
+  describe "take_concentrations/2" do
+    @grid [
+      [5, 3, 1, 2],
+      [4, 1, 1, 3],
+      [2, 3, 2, 4],
+      [0, 2, 3, 4]
+    ]
+
+    test "returns the concentration for all locations in the grid, with index" do
+      assert [
+               [2, 2, score: 23],
+               [1, 1, score: 22],
+               [1, 2, score: 20],
+               [1, 0, score: 18],
+               [2, 1, score: 18],
+               [3, 2, score: 18],
+               [2, 3, score: 17],
+               [0, 1, score: 15],
+               [0, 0, score: 13],
+               [1, 3, score: 13],
+               [3, 3, score: 13],
+               [2, 0, score: 12],
+               [3, 1, score: 12],
+               [0, 2, score: 11],
+               [0, 3, score: 7],
+               [3, 0, score: 7]
+             ] == WaterRover.take_concentrations(@grid, 16)
+    end
+
+    test "returns the 3 biggest concentration in the grid, with index" do
+      assert [
+               [2, 2, score: 23],
+               [1, 1, score: 22],
+               [1, 2, score: 20]
+             ] == WaterRover.take_concentrations(@grid, 3)
+    end
 end
