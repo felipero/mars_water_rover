86da86c0b2ef4858ea858957f64ec5bc37ae24b3
Felipe Rodrigues <felipe@crafters.cc>
2020-02-09 21:42:22 -0300

    Fixing a nasty bug due to typo in the surroundings and coincidence in the tests.

--- lib/water_rover.ex
+++ lib/water_rover.ex
@@ -5,33 +5,69 @@ defmodule WaterRover do
 
   @doc """
   Returns a list containing the `count` biggest concentrations in the `grid` with the index in the format [x, y, score: sum].
+  @doc """
+  Returns a list containing the concentrations in the `grid` with the index, ordered by the biggest concentrations.
+  The return will be in the format [x, y, score: sum].
+
+  ## Examples
+      iex> grid = [[5, 3, 1, 2], [4, 1, 1, 3], [2, 3, 2, 4], [0, 2, 3, 4]]
+      iex> WaterRover.take_concentrations(grid)
+      [
+        [2, 2, %{score: 23}],
+        [1, 1, %{score: 22}],
+        [2, 1, %{score: 20}],
+        [0, 1, %{score: 18}],
+        [1, 2, %{score: 18}],
+        [2, 3, %{score: 18}],
+        [3, 2, %{score: 17}],
+        [1, 0, %{score: 15}],
+        [0, 0, %{score: 13}],
+        [3, 1, %{score: 13}],
+        [3, 3, %{score: 13}],
+        [0, 2, %{score: 12}],
+        [1, 3, %{score: 12}],
+        [2, 0, %{score: 11}],
+        [0, 3, %{score: 7}],
+        [3, 0, %{score: 7}]
+      ]
+  """
+  def take_concentrations(grid = [[_ | _] | _]) do
+    for {cols, row} <- Enum.with_index(grid),
+        {_, col} <- Enum.with_index(cols),
+        into: Heap.new(&comparator/2) do
+      [
+        col,
+        row,
+        %{score: grid |> surroundings(row, col) |> Enum.sum()}
+      ]
+    end
+    |> Enum.to_list()
+  end
+
+  @doc """
+  Returns a list containing the `count` biggest concentrations in the `grid` with the index in the format [x, y, score: sum].
 
   ## Examples
       iex> grid = [[5, 3, 1, 2], [4, 1, 1, 3], [2, 3, 2, 4], [0, 2, 3, 4]]
       iex> WaterRover.take_concentrations(grid, 3)
       [
-        [2, 2, score: 23],
-        [1, 1, score: 22],
-        [1, 2, score: 20]
+        [2, 2, %{score: 23}],
+        [1, 1, %{score: 22}],
+        [1, 2, %{score: 20}]
       ]
   """
-  def take_concentrations(grid = [[_ | _] | _], count) do
-    for {row, row_idx} <- Enum.with_index(grid),
-        {_, col} <- Enum.with_index(row),
-        into: Heap.new(&comparator/2) do
-      [
-        row_idx,
-        col,
-        score: grid |> surroundings(row_idx, col) |> Enum.sum()
-      ]
-    end
-    |> Enum.take(count)
-  end
+  def take_concentrations(grid = [[_ | _] | _], count),
+    do: take_concentrations(grid) |> Enum.take(count)
 
+  @doc """
+  Returns an empty list when the grid is empty or filled with empty lists.
+  """
   def take_concentrations(_, _), do: []
 
   defp comparator(first, second),
-    do: {Enum.at(first, 2), -Enum.at(first, 0)} >= {Enum.at(second, 2), -Enum.at(second, 0)}
+    do:
+      {Enum.at(first, 2), -Enum.at(first, 0), -Enum.at(first, 1)} >=
+        {Enum.at(second, 2), -Enum.at(second, 0), -Enum.at(second, 1)}
 
   defp surroundings(grid, row, col) do
     size = Enum.count(grid)
@@ -40,13 +76,13 @@ defmodule WaterRover do
 
     [
       grid |> Enum.at(left_idx, []) |> Enum.at(top_idx, 0),
-      grid |> Enum.at(row, []) |> Enum.at(top_idx, 0),
-      grid |> Enum.at(row + 1, []) |> Enum.at(top_idx, 0),
       grid |> Enum.at(left_idx, []) |> Enum.at(col, 0),
-      grid |> Enum.at(row, []) |> Enum.at(col, 0),
-      grid |> Enum.at(row + 1, []) |> Enum.at(col, 0),
       grid |> Enum.at(left_idx, []) |> Enum.at(col + 1, 0),
+      grid |> Enum.at(row, []) |> Enum.at(top_idx, 0),
+      grid |> Enum.at(row, []) |> Enum.at(col, 0),
       grid |> Enum.at(row, []) |> Enum.at(col + 1, 0),
+      grid |> Enum.at(row + 1, []) |> Enum.at(top_idx, 0),
+      grid |> Enum.at(row + 1, []) |> Enum.at(col, 0),
       grid |> Enum.at(row + 1, []) |> Enum.at(col + 1, 0)
     ]
   end
--- test/water_rover_test.exs
+++ test/water_rover_test.exs
@@ -5,40 +5,45 @@ defmodule WaterRoverTest do
   @moduledoc false
 
   describe "take_concentrations/2" do
-    @grid [
-      [5, 3, 1, 2],
-      [4, 1, 1, 3],
-      [2, 3, 2, 4],
-      [0, 2, 3, 4]
-    ]
 
     test "returns the concentration for all locations in the grid, with index" do
+      grid = [
+        [5, 3, 1, 2],
+        [4, 1, 1, 3],
+        [2, 3, 2, 4],
+        [0, 2, 3, 4]
+      ]
+
       assert [
-               [2, 2, score: 23],
-               [1, 1, score: 22],
-               [1, 2, score: 20],
-               [1, 0, score: 18],
-               [2, 1, score: 18],
-               [3, 2, score: 18],
-               [2, 3, score: 17],
-               [0, 1, score: 15],
-               [0, 0, score: 13],
-               [1, 3, score: 13],
-               [3, 3, score: 13],
-               [2, 0, score: 12],
-               [3, 1, score: 12],
-               [0, 2, score: 11],
-               [0, 3, score: 7],
-               [3, 0, score: 7]
-             ] == WaterRover.take_concentrations(@grid, 16)
+               [2, 2, %{score: 23}],
+               [1, 1, %{score: 22}],
+               [2, 1, %{score: 20}],
+               [0, 1, %{score: 18}],
+               [1, 2, %{score: 18}],
+               [2, 3, %{score: 18}],
+               [3, 2, %{score: 17}],
+               [1, 0, %{score: 15}],
+               [0, 0, %{score: 13}],
+               [3, 1, %{score: 13}],
+               [3, 3, %{score: 13}],
+               [0, 2, %{score: 12}],
+               [1, 3, %{score: 12}],
+               [2, 0, %{score: 11}],
+               [0, 3, %{score: 7}],
+               [3, 0, %{score: 7}]
+             ] == WaterRover.take_concentrations(grid)
     end
 
     test "returns the 3 biggest concentration in the grid, with index" do
       assert [
-               [2, 2, score: 23],
-               [1, 1, score: 22],
-               [1, 2, score: 20]
-             ] == WaterRover.take_concentrations(@grid, 3)
+               [1, 2, %{score: 27}],
+               [1, 1, %{score: 25}],
+               [2, 2, %{score: 23}]
+             ] ==
+               WaterRover.take_concentrations(
+                 [[2, 3, 2, 1], [4, 4, 2, 0], [3, 4, 1, 1], [2, 3, 4, 4]],
+                 3
+               )
     end
 
     test "returns empty for empty grid" do
